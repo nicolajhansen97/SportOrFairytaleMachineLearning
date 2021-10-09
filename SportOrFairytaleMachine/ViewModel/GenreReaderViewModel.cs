0632f1ca40041f30c6a61b2fa2df93fad5af1867
Nicolaj Hansen <nicolajhansen97@live.dk>
2021-10-09 17:56:43 +0200

    MVVM pattern made totally ready

---
+++ SportOrFairytaleMachine/ViewModel/GenreReaderViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using SportOrFairytaleMachine.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace SportOrFairytaleMachine.ViewModel
+{
+    class GenreReaderViewModel : Bindable
+    {
+
+        public DelegateCommand openFileChooser { get; set; }
+
+        private string _loadFile = "Load file";
+
+        public string LoadFile
+        {
+            get { return _loadFile; }
+            set { _loadFile = value; propertyIsChanged(); }
+        }
+
+        public GenreReaderViewModel()
+        {
+            openFileChooser = new DelegateCommand(o =>
+                {
+                    FileChooser();
+                });
+
+        }
+
+        private void FileChooser()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string textTest = File.ReadAllText(openFileDialog.FileName);
+                MessageBox.Show(textTest);
+            }
+        }
+    }
+}
---
+++ SportOrFairytaleMachine/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportOrFairytaleMachine.Model;
+
+namespace SportOrFairytaleMachine.ViewModel
+{
+    /// <summary>
+    /// a baseclass for changing viewmodels, but here it only has one. 
+    /// </summary>
+    class MainViewModel : Bindable
+    {
+        /// <summary>
+        /// A getter and setter for my viewmodel
+        /// </summary>
+        public GenreReaderViewModel TDFVM { get; set; }
+        /// <summary>
+        /// a private instacnce of my current view
+        /// </summary>
+        private object _currentView;
+        /// <summary>
+        /// a complete getter and setter with propyischanged to dectect if i change viewmodel.
+        /// </summary>
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; propertyIsChanged(); }
+        }
+        /// <summary>
+        /// The constructor that is used to new the viewmodel and make the currentview into the viewmodel.
+        /// </summary>
+        public MainViewModel()
+        {
+            TDFVM = new GenreReaderViewModel();
+            CurrentView = TDFVM;
+        }
+    }
+}

3ecd3a86e4d2492532235c0a2290dd5a91b0856f
Nicolaj Hansen <nicolajhansen97@live.dk>
2021-10-13 14:05:15 +0200

    Made data class, made distance vector, prepared a lot of stuff.

---
+++ SportOrFairytaleMachine/Model/DistanceModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportOrFairytaleMachine.Model
+{
+    class DistanceModel : Bindable
+    {
+        private double sum;
+
+        public double Sum
+        {
+            get { return sum; }
+            set { sum = value; propertyIsChanged(); }
+        }
+
+        private double distance;
+
+        public double Distance
+        {
+            get { return distance; }
+            set { distance = value; propertyIsChanged(); }
+        }
+
+        private string type;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; propertyIsChanged(); }
+        }
+
+
+
+    }
+}
--- SportOrFairytaleMachine/ViewModel/GenreReaderViewModel.cs
+++ SportOrFairytaleMachine/ViewModel/GenreReaderViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using SportOrFairytaleMachine.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,13 +16,27 @@ namespace SportOrFairytaleMachine.ViewModel
     class GenreReaderViewModel : Bindable
     {
 
+        bool[] vectorArray;
+        bool[] unknownVectorArray;
+        List<double> distanceSortList = new List<double>();
+
+
+        private ObservableCollection<DistanceModel> distanceList = new ObservableCollection<DistanceModel>();
+        public ObservableCollection<DistanceModel> DistanceList
+        {
+            get { return distanceList; }
+            set { distanceList = value; propertyIsChanged(); }
+        }
+
+        List<string> AllWordsDictonary = new List<string>();
+
         string readContents = "";
-        string dictoryFile = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory) + "/DictoryBag.txt";
+        string dictoryFile = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory) + "/TextReader/DictoryBag.txt";
         string desktop = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory);
-        string[] path = { "/Eventyr/Fairytale1.txt", "/Eventyr/Fairytale2.txt", "/Eventyr/Fairytale3.txt", "/Eventyr/Fairytale4.txt", "/Eventyr/Fairytale5.txt",
-                          "/Eventyr/Fairytale6.txt", "/Eventyr/Fairytale7.txt", "/Eventyr/Fairytale8.txt", "/Eventyr/Fairytale9.txt", "/Eventyr/Fairytale10.txt",
-                          "/Sport/Sport1.txt", "/Sport/Sport2.txt", "/Sport/Sport3.txt", "/Sport/Sport4.txt", "/Sport/Sport5.txt", "/Sport/Sport6.txt",
-                          "/Sport/Sport7.txt", "/Sport/Sport8.txt", "/Sport/Sport9.txt", "/Sport/Sport10.txt",};
+        string[] path = { "/TextReader/Eventyr/Fairytale1.txt", "/TextReader/Eventyr/Fairytale2.txt", "/TextReader/Eventyr/Fairytale3.txt", "/TextReader/Eventyr/Fairytale4.txt", "/TextReader/Eventyr/Fairytale5.txt",
+                          "/TextReader/Eventyr/Fairytale6.txt", "/TextReader/Eventyr/Fairytale7.txt", "/TextReader/Eventyr/Fairytale8.txt", "/TextReader/Eventyr/Fairytale9.txt", "/TextReader/Eventyr/Fairytale10.txt",
+                          "/TextReader/Sport/Sport1.txt", "/TextReader/Sport/Sport2.txt", "/TextReader/Sport/Sport3.txt", "/TextReader/Sport/Sport4.txt", "/TextReader/Sport/Sport5.txt", "/TextReader/Sport/Sport6.txt",
+                          "/TextReader/Sport/Sport7.txt", "/TextReader/Sport/Sport8.txt", "/TextReader/Sport/Sport9.txt", "/TextReader/Sport/Sport10.txt",};
 
 
         public DelegateCommand openFileChooser { get; set; }
@@ -43,23 +59,65 @@ namespace SportOrFairytaleMachine.ViewModel
 
         public GenreReaderViewModel()
         {
+            readAllTexts();
+
+
             openFileChooser = new DelegateCommand(o =>
                 {
-                    //FileChooser();
-                    //TextSplitter();
-                    readAllTexts();
+                    FileChooser();  
                 });
 
         }
 
         private void FileChooser()
         {
+            distanceList.Clear();
+
+            string fixedInput = "";
+            string textTest = "";
+            List<string> unknownTextWordList = new List<string>();
+            List<bool> unknownVectorList = new List<bool>();
+
+
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
-                string textTest = File.ReadAllText(openFileDialog.FileName);
-                MessageBox.Show(textTest);
+                textTest = File.ReadAllText(openFileDialog.FileName);
             }
+
+            //This will remove all ., etc which we dont want into our list. It will also split the text so only 1 word will be on each line.
+            fixedInput = Regex.Replace(textTest, "[^a-zA-Z0-9% ._]", string.Empty);
+            fixedInput = fixedInput.Replace(".", string.Empty);
+            var punctuation = fixedInput.Where(Char.IsPunctuation).ToArray();
+            var words = fixedInput.Split().Distinct().Select(x => x.Trim(punctuation));
+
+            using (StreamWriter sw = File.CreateText(desktop + "/TextReader/TestUnknown.txt"))
+            {
+                sw.WriteLine(string.Join(Environment.NewLine, words));
+            }
+
+            string[] unknownText = File.ReadAllLines(desktop + "/TextReader/TestUnknown.txt");
+
+            foreach (string unknownTextLine in unknownText)
+            {
+                unknownTextWordList.Add(unknownTextLine);
+            }
+
+            foreach (string line in AllWordsDictonary)
+            {
+                if (unknownTextWordList.Contains(line))
+                {
+                    unknownVectorList.Add(true);
+                }
+                else
+                {
+                    unknownVectorList.Add(false);
+                }
+            }
+            unknownVectorArray = unknownVectorList.ToArray();
+           
+            vectorTheTexts();
+
         }
 
         //This method will split the text, but also clean it and prepare it for vectors.
@@ -81,6 +139,8 @@ namespace SportOrFairytaleMachine.ViewModel
             var punctuation = fixedInput.Where(Char.IsPunctuation).ToArray();
             var words = fixedInput.Split().Distinct().Select(x => x.Trim(punctuation));
 
+
+
           
             //Deletes the file as we dont need it anymore.
             File.Delete(dictoryFile);
@@ -90,7 +150,14 @@ namespace SportOrFairytaleMachine.ViewModel
             {
                 sw.WriteLine(string.Join(Environment.NewLine, words));
             }
-               TextBox = string.Join(Environment.NewLine, words);
+
+            string[] lines = File.ReadAllLines(dictoryFile);
+
+            foreach (string line in lines)
+            {
+                AllWordsDictonary.Add(line);
+            }
+            TextBox = string.Join(Environment.NewLine, words);
         }
 
         //This method will read all textes and save it all into one single file which will be used as the dictonary. 
@@ -119,6 +186,118 @@ namespace SportOrFairytaleMachine.ViewModel
 
             }
                 TextSplitter();
+        }
+
+        private void vectorTheTexts()
+        {
+
+            string singleTextFixedInput = "";
+            string typeOfText = "";
+
+            List<bool> vectorList = new List<bool>();
+           
+            List<string> fairyTaleWordList = new List<string>();
+            string[] pathFairyTale = { "/TextReader/Eventyr/Fairytale1.txt", "/TextReader/Eventyr/Fairytale2.txt", "/TextReader/Eventyr/Fairytale3.txt", "/TextReader/Eventyr/Fairytale4.txt", "/TextReader/Eventyr/Fairytale5.txt",
+                          "/TextReader/Eventyr/Fairytale6.txt", "/TextReader/Eventyr/Fairytale7.txt", "/TextReader/Eventyr/Fairytale8.txt", "/TextReader/Eventyr/Fairytale9.txt", "/TextReader/Eventyr/Fairytale10.txt",
+                          "/TextReader/Sport/Sport1.txt", "/TextReader/Sport/Sport2.txt", "/TextReader/Sport/Sport3.txt", "/TextReader/Sport/Sport4.txt", "/TextReader/Sport/Sport5.txt", "/TextReader/Sport/Sport6.txt",
+                          "/TextReader/Sport/Sport7.txt", "/TextReader/Sport/Sport8.txt", "/TextReader/Sport/Sport9.txt", "/TextReader/Sport/Sport10.txt",};
+
+
+
+            foreach (string fairyTale in pathFairyTale)
+            {
+                vectorList.Clear();
+                fairyTaleWordList.Clear();
+
+                if(fairyTale.Contains("Fairytale"))
+                {
+                    typeOfText = "Fairytale";
+                }
+                else
+                {
+                    typeOfText = "Sport";
+                }
+
+                using (StreamReader streamReader2 = new StreamReader(desktop + fairyTale))
+                {
+                    singleTextFixedInput = streamReader2.ReadToEnd();
+                }
+
+                singleTextFixedInput = Regex.Replace(singleTextFixedInput, "[^a-zA-Z0-9% ._]", string.Empty);
+                singleTextFixedInput = singleTextFixedInput.Replace(".", string.Empty);
+                var punctuation = singleTextFixedInput.Where(Char.IsPunctuation).ToArray();
+                var words = singleTextFixedInput.Split().Distinct().Select(x => x.Trim(punctuation));
+
+                if (File.Exists(desktop + fairyTale + "Vector.txt"))
+                {
+                    File.Delete(desktop + fairyTale + "Vector.txt");
+                }
+                if (File.Exists(desktop + "/TextReader/TempList.txt")) 
+                {
+                    File.Delete(desktop + "/TextReader/TempList.txt");
+                }
+
+                using (StreamWriter sw2 = File.CreateText(desktop + "/TextReader/TempList.txt"))
+                {
+                    sw2.WriteLine(string.Join(Environment.NewLine, words));
+                }
+
+                string[] fairyTaleLines = File.ReadAllLines(desktop + "/TextReader/TempList.txt");
+
+                foreach (string fairyTaleline in fairyTaleLines)
+                {
+                    fairyTaleWordList.Add(fairyTaleline);
+                }
+
+                foreach (string line in AllWordsDictonary)
+                    {
+                    if (fairyTaleWordList.Contains(line))
+                    {
+                        vectorList.Add(true);
+                    }
+                    else
+                    {
+                        vectorList.Add(false);
+                    }
+                    }
+                vectorArray = vectorList.ToArray();
+               
+                DistanceList.Add(CalculateInfo(vectorArray, unknownVectorArray, typeOfText));
+
+
+                using (var file = new StreamWriter(desktop + fairyTale + "Vector.txt"))
+                {
+                    vectorList.ForEach(v => file.WriteLine(v));
+                }
+            }
+        }
+        private DistanceModel CalculateInfo (bool[] textArray, bool[] uknownTextArray, string StoryType)
+        {
+            DistanceModel dm = new DistanceModel();
+
+            for (int i = 0; i < AllWordsDictonary.Count; i++)
+            {
+                dm.Sum += Math.Pow((vectorArray[i] ? 1 : 0) - (unknownVectorArray[i] ? 1 : 0), 2);
+            }
+
+            double distance = Math.Sqrt(dm.Sum);
+            dm.Distance = distance;
+            dm.Type = StoryType;
+            distanceSortList.Add(distance);
+
+            knnAlgo();
+            return dm;
+        }
+
+        private void knnAlgo()
+        {
+            distanceSortList = distanceSortList.OrderBy(p => p).ToList();
+            for (int i = 0; i < 6; i++)
+            {
+
+            }
+            
+
         }
     }
 }
